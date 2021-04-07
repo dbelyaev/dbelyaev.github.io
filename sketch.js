ab4bcec2e36eaf8e76d5397602b6a30ac18030fa
Dmitriy Belyaev <me@dmitriy.io>
2021-04-07 22:34:44 +0200

    having fun with walker

--- sketch.js
+++ sketch.js
@@ -1,7 +1,37 @@
+var w;
+
 function setup() {
+	w = new Walker();
+
 	createCanvas(windowWidth, windowHeight);
+	background(51)
 }
 
 function draw() {
-	background(245, 245, 220)
+	w.update();
+	w.draw();
+}
+
+function Walker() {
+	this.x = windowWidth * .5;
+	this.y = windowHeight * .5;
+	this.size = random(max(windowWidth, windowHeight) * .001, max(windowWidth, windowHeight) * .01);
+
+	this.update = function () {
+		this.step();
+	}
+
+	this.draw = function () {
+		stroke('white');
+		//point(this.x, this.y)
+		ellipse(this.x, this.y, this.size, this.size);
+	}
+
+	this.step = function () {
+		var stepX = random(-1, 1);
+		var stepY = random(-1, 1);
+
+		this.x += stepX * this.size;
+		this.y += stepY * this.size;
+	}
 }
